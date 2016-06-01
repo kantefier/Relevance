e191e6662d526ba2262d82af98ac1922a31bdec7
Kirill Nebogin <kinebogin@gmail.com>
2016-06-01 17:32:59 +0300

    pure part modifications

--- Relevance.scala
+++ Relevance.scala
@@ -16,21 +16,30 @@ object Relevance {
 		// use Pearson's metric to measure the "distance" between two user's musical tastes
 		def similarity(x: User, y: User): Double = {
 			// collect common artists from users' listening libraries
-			val commonArtistsX = x.library.filter(artInfo => y.library.map(_.name).contains(artInfo.name))
-			val commonArtistsY = y.library.filter(artInfo => commonArtistsX.map(_.name).contains(artInfo.name))
-			??? TODO
+			val commonArtistsX: List[ArtistInfo] = x.library.filter(artInfo => y.library.map(_.name).contains(artInfo.name))
+			val commonArtistsY: List[ArtistInfo] = y.library.filter(artInfo => commonArtistsX.map(_.name).contains(artInfo.name))
+
+			// common artists count
+			val N = commonArtistsX.length
+			// TODO: return 0 if N == 0
+
+			val sumX = commonArtistsX.map(_.plays).sum
+			val sumY = commonArtistsY.map(_.plays).sum
+
+			// TODO
+			0.0
 		}
 	}
 
 	///
 
 	/// pure part
-	def findMostRelevant[T : Metric](base: T, others: List[T]): T = {
+	def findMostRelevant[T](base: T, others: List[T])(implicit ev: Metric[T]): T = {
 		//inner tail recursive function
 		def innerIter(others: List[T], bestSoFar: T, bestMetric: Double): T = others match {
 			case Nil => bestSoFar
 			case current :: others =>
-				val currentSim = similarity(base, current)
+				val currentSim = ev.similarity(base, current)
 				if(currentSim > bestMetric)
 					innerIter(others, current, currentSim)
 				else
@@ -40,7 +49,7 @@ object Relevance {
 		others match {
 			// if there's no others, the best match to a user is himself
 			case Nil => base
-			case x :: others => innerIter(others, x, similarity(base, x))
+			case x :: others => innerIter(others, x, ev.similarity(base, x))
 		}
 	}
 }
