e5c40cd7f87c7dbb43cc1bca36f43c6453ee8b95
Kirill Nebogin <kinebogin@gmail.com>
2016-06-01 14:25:30 +0300

    pure part almost done

--- Relevance.scala
+++ Relevance.scala
@@ -4,15 +4,43 @@ object Relevance {
 	}
 
 	///
+	// define some domain classes
 	case class User(ident: String, library: List[ArtistInfo])
 	case class ArtistInfo(name: String, mbid: Option[String], plays: Int)
+	///
+
+	trait Metric[T] {
+		def similarity(x: T, y: T): Double
+	}
+	implicit object MetricForUser extends Metric[User] {
+		// use Pearson's metric to measure the "distance" between two user's musical tastes
+		def similarity(x: User, y: User): Double = {
+			// collect common artists from users' listening libraries
+			val commonArtistsX = x.library.filter(artInfo => y.library.map(_.name).contains(artInfo.name))
+			val commonArtistsY = y.library.filter(artInfo => commonArtistsX.map(_.name).contains(artInfo.name))
+			??? TODO
+		}
+	}
+
+	///
 
 	/// pure part
-	def similarity(usr1: User, usr2: User): Int = {
-		// общие артисты из списка первого пользователя
-		val commonArtists1 = usr1.library.filter(artInfo => usr2.library.map(_.name).contains(artInfo.name))
-		// общие артисты из списка второго пользователя
-		val commonArtists2 = usr2.library.filter(artInfo => usr1.library.map(_.name).contains(artInfo.name))
+	def findMostRelevant[T : Metric](base: T, others: List[T]): T = {
+		//inner tail recursive function
+		def innerIter(others: List[T], bestSoFar: T, bestMetric: Double): T = others match {
+			case Nil => bestSoFar
+			case current :: others =>
+				val currentSim = similarity(base, current)
+				if(currentSim > bestMetric)
+					innerIter(others, current, currentSim)
+				else
+					innerIter(others, bestSoFar, bestMetric)
+		}
 
+		others match {
+			// if there's no others, the best match to a user is himself
+			case Nil => base
+			case x :: others => innerIter(others, x, similarity(base, x))
+		}
 	}
 }
