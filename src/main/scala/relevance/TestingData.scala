20c0ba9465f5646b7be550886ba886df629b231d
Gravity Blaster <droptheself@hotmail.com>
2016-06-24 00:00:42 +0400

    Tweaking tests

--- src/main/scala/relevance/TestingData.scala
+++ src/main/scala/relevance/TestingData.scala
@@ -24,6 +24,10 @@ object TestingData {
         randomStringFromChars(('a' to 'z') ++ ('0' to '9'), 20)
     }.toList
 
+    def genArtists(count: Int): List[String] = (1 to count).map {_ =>
+        randomStringFromChars(('a' to 'z') ++ ('0' to '9'), 20)
+    }.toList
+
     /**
      * Fifry lines of testing data, same format as in the dataset
      */
@@ -31,6 +35,19 @@ object TestingData {
         val userIdent = randomStringFromChars(('a' to 'z') ++ ('0' to '9'), 40)
         testingArtists.map { artist =>
             userIdent + " iywgeiu " + artist + " " + rand.nextInt(200)
+        }
+    }.toList
+
+    /**
+    *   Generate lines of testing data for given user count and artist count
+    */
+    def testingUserData(userCount: Int, artistCount: Int): List[String] = {
+        val artists: List[String] = genArtists(artistCount)
+        (1 to userCount).flatMap { _ =>
+            val userIdent = randomStringFromChars(('a' to 'z') ++ ('A' to 'Z') ++ ('0' to '9'), 40)
+            artists.map { artist =>
+                userIdent + " qwertyMBIDqwerty " + artist + " " + rand.nextInt(2000)
+            }
         }
     }.toList
 }
