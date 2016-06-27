bcf91134cc50111f3d142cdf6ce98cf963e63631
kantefier <kinebogin@gmail.com>
2016-06-28 01:38:29 +0400

    yet another dummy commit

--- src/main/scala/relevance/TestingData.scala
+++ src/main/scala/relevance/TestingData.scala
@@ -67,7 +67,7 @@ object TestingData {
     /**
     *   Writes infinite stream to file
     *   TODO: fix "GC overhead limit exceeded" bug for big line numbers
-    *       Possible cause: maybe the infinite stream doesn't clear up and just grows in the memory as it evaluates
+    *       Possible cause: the infinite stream doesn't clear up and just grows in the memory as it evaluates
     */
     def streamToFile(stream: Stream[String], filePath: String, maxLines: Int, chunkSize: Int = 50): Unit = {
         import java.io._
